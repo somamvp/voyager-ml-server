632b466f8288317221a77f589609840e10dd16f1
woosukji <woosukji@snu.ac.kr>
2022-08-16 17:03:07 +0900

    add run script with target params & log datetime

--- run_server.sh
+++ run_server.sh
@@ -9,7 +9,14 @@ cd $BASEDIR
 conda activate server-gpu-env
 conda info | grep "active environment"
 
+if [ -z $1 ]; then 
+    SERVER_SCRIPT='server.py'
+else 
+    SERVER_SCRIPT=$1
+fi
+
 [ ! -d "runs" ] && mkdir runs
-python server.py 1 > runs/run.log 2>&1
+echo "running script ${SERVER_SCRIPT}"
+python $SERVER_SCRIPT 1 > runs/$(date +%y-%d-%m_%T).log 2>&1
 
 echo "server terminated!"
