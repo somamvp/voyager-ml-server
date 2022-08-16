adf8274015ea554aaf570d210bade077912d90cc
woosukji <woosukji@snu.ac.kr>
2022-08-16 15:16:39 +0900

    add server run script

---
+++ server_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# export PATH="${PATH}:/home/ubuntu/miniconda3/bin:/home/ubuntu/miniconda3/condabin:"
+source ~/miniconda3/etc/profile.d/conda.sh
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+conda activate server-gpu-env
+
+conda info
+
+[ ! -d "runs" ] && mkdir runs
+python server.py 1 > runs/run.log 2>&1
+
+echo "Done!"
