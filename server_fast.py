fb81e8a579bfe81a3ac93e4a7588f5e5f724df53
juwon <juwon8891@gmail.com>
2022-09-07 14:25:30 +0900

    🔊 [MVP]로그 사용법 변경

--- server_fast.py
+++ server_fast.py
@@ -3,6 +3,7 @@ from PIL import Image
 from StateMachine import StateMachine
 from model import get_model
 import os, io, time, json
+from loguru import logger
 
 app = FastAPI()
 stateMachine = StateMachine()
@@ -25,20 +26,20 @@ async def file_upload(img: bytes = File(...), sequenceNo: int = 1):
   im_id = sequenceNo
 
   im = Image.open(io.BytesIO(im_file))
-  # print(f"image recieved! size: {im.size}, image conversion time: {time.time() - tick}")
+  # logger.info(f"image recieved! size: {im.size}, image conversion time: {time.time() - tick}")
 
   model_tick = time.time()
   results = model(im, size=640)
   model_runtime = time.time() - model_tick
   results.save(save_dir=f"run_imgs/{im_id}")
   im.save(f"run_imgs/{im_id}/input.png", "PNG")
-  print(f"model runtime: {model_runtime}")
+  logger.info("model runtime: {}", model_runtime)
   
   result_dict = results.pandas().xyxy[0].to_dict(orient="records")
-  print(f"발견된 물체: {[ result['name'] for result in result_dict ]}")
+  logger.info("발견된 물체: {}", [ result['name'] for result in result_dict ])
 
   stateMachine.newFrame(result_dict)
-  print(f"사용자 안내: {stateMachine.guides}")
+  logger.info("사용자 안내: {}", stateMachine.guides)
 
   # return_dict = {
   #   "yolo": result_dict,
@@ -49,13 +50,13 @@ async def file_upload(img: bytes = File(...), sequenceNo: int = 1):
 
   return_json = json.dumps(stateMachine.guides)
 
-  print(f"total runtime: {time.time() - tick}", flush=True)
+  logger.info("total runtime: {}", (time.time() - tick))
   return return_json
 
 
 @app.get("//start")
 async def start():
-    print("--------restarting state machine----------")
+    logger.info("--------restarting state machine----------")
     global stateMachine
     stateMachine = StateMachine()
     return ""
