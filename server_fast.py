baf3920ef2fd50bd5807670bf8f7f102956da956
biancco <biancco@snu.ac.kr>
2022-10-03 22:31:47 +0900

    FIX : yolo_box generating problem resolved

--- server_fast.py
+++ server_fast.py
@@ -18,6 +18,30 @@ opt = easydict.EasyDict({'agnostic_nms':False, 'augment':True, 'classes':None, '
 app = FastAPI()
 stateMachine = StateMachine()
 detector = Detector(opt)
+result_dict={}  # 세션NO : detection result, 서비스 배포 시 여러장의 이미지를 한꺼번에 추론시키는 경우를 대비해 구축해놓았음.
+                                            # 추후 메모리 누수 막기 위해 초기화시키는 알고리즘 필요
+
+def bytes2cv(source: bytes):
+    encoded_img = np.fromstring(source, dtype = np.uint8)  # type : nparray
+    RGBD = cv2.imdecode(encoded_img, cv2.IMREAD_UNCHANGED)
+    if is_Rot:
+        RGBD = np.rot90(RGBD, 3)
+    channel = RGBD.shape[2]
+    # print(RGBD.shape)
+
+    depth_cv = None
+    if channel == 4:    # 4-channel image        
+        # Premultiplied Resolving
+        # RGBD = cv2.cvtColor(RGBD, cv2.COLOR_mRGBA2RGBA)
+        depth_cv = (RGBD[:,:,3])
+        img_cv = (RGBD[:,:,0:3])    
+        img_cv = np.ascontiguousarray(img_cv, dtype=np.uint8)       
+        print(f"Depth-integrated image recieved, depth value max: {depth_cv.max()}, min: {depth_cv.min()}")
+
+    elif channel == 3:  # 3-channel image
+        img_cv = RGBD
+
+    return img_cv, depth_cv
 
 @app.get("/")
 async def root():
@@ -28,59 +52,55 @@ async def test():
     return {"message": "test"}
 
 @app.post("/upload")
-async def file_upload(source: bytes = File(...), sequenceNo: int = 1):
+async def file_upload(source: bytes = File(...), SESSION_NO: int = 1):
+    # High-frequency Acting
     tick = time.time()
-    result_dict={}  # sequenceNO : detection result
-    im_id = sequenceNo
+    high_freq = True
 
     # 이미지 로딩
-    encoded_img = np.fromstring(source, dtype = np.uint8)  # type : nparray
-    RGBD = cv2.imdecode(encoded_img, cv2.IMREAD_UNCHANGED)
-    if is_Rot:
-        RGBD = np.rot90(RGBD, 3)
-    channel = RGBD.shape[2]
-    print(RGBD.shape)
-
-    depth_cv = None
-    if channel == 4:    # 4-channel image
-        print("Depth-integrated image recieved")
-        
-        # Premultiplied Resolving
-        # RGBD = cv2.cvtColor(RGBD, cv2.COLOR_mRGBA2RGBA)
-
-        depth_cv = (RGBD[:,:,3])
-        img_cv = (RGBD[:,:,0:3])   
-        
-
-        print(f"depth image max: {depth_cv.max()}, min: {depth_cv.min()}")
-    elif channel == 3:  # 3-channel image
-        img_cv = RGBD
-    else:   # channel error
-        print(f"Image Channel ERROR seqNO : {sequenceNo}")
-
-
-    rgb = img_cv
+    rgb, depth_cv = bytes2cv(source)
     logger.info(f"image recieved! size: {rgb.size}, image conversion time: {time.time() - tick}")
-
+    print(rgb.shape)
 
     # YOLO 추론
-    result_dict[im_id] = detector.inference(rgb, im_id, depth_cv)  # 리턴타입은 {'yolo':list of bbox}, 바운딩박스 자료형은 딕셔너리    
+    result_dict[SESSION_NO] = detector.inference(rgb, SESSION_NO, depth_cv)  # 리턴타입은 {'yolo':list of bbox}, 바운딩박스 자료형은 딕셔너리   
+
+
     logger.info(f'Inference Done. ({time.time()- tick:.3f}s)')
-    
+    logger.info("세션 아이디: {}", SESSION_NO)
+    logger.info("발견된 물체: {}", [ result['name'] for result in result_dict[SESSION_NO]['yolo'] ])
 
 
-    # 결과 출력
-    logger.info("세션 아이디: {}", im_id)
-    logger.info("발견된 물체: {}", [ result['name'] for result in result_dict[im_id]['yolo'] ])
-
-    stateMachine.newFrame(result_dict[im_id]['yolo'])
+    # StateMachine
+    stateMachine.newFrame(result_dict[SESSION_NO]['yolo'])
     logger.info("사용자 안내: {}", stateMachine.guides)
 
     guide_enum = json.dumps(stateMachine.guides)
 
 
-    logger.info("total runtime: {}", (time.time() - tick))
-    return guide_enum, result_dict[im_id]
+    logger.info("/upload total runtime: {}", (time.time() - tick))
+    return high_freq, result_dict[SESSION_NO], guide_enum
+
+@app.post("/update")
+async def file_update(source: bytes = File(...), SESSION_NO: int = 1):
+    # Low-frequency Acting
+    tick = time.time()
+    high_freq = False
+
+    # 이미지 로딩
+    rgb, depth_cv = bytes2cv(source)
+    logger.info(f"image recieved! size: {rgb.size}, image conversion time: {time.time() - tick}")
+
+    # YOLO 추론
+    result_dict[SESSION_NO] = detector.inference(rgb, SESSION_NO, depth_cv)  # 리턴타입은 {'yolo':list of bbox}, 바운딩박스 자료형은 딕셔너리   
+
+
+    logger.info(f'Inference Done. ({time.time()- tick:.3f}s)')
+    logger.info("세션 아이디: {}", SESSION_NO)
+    logger.info("발견된 물체: {}", [ result['name'] for result in result_dict[SESSION_NO]['yolo'] ])
+
+    logger.info("/update total runtime: {}", (time.time() - tick))
+    return high_freq, result_dict[SESSION_NO]
 
 
 @app.get("/start")
