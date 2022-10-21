5dc126c088e7340aabe12c7916360857fb3f7983
woosukji <woosukji@snu.ac.kr>
2022-10-21 22:56:10 +0900

    [MVP] add: enhance mock video testing script

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 import sys, os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+path = os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-sys.path.append("./yolov7/")
+sys.path.append('./yolov7')
--- yolo_tracking_video_test.py
+++ yolo_tracking_video_test.py
@@ -1,22 +1,48 @@
-from fastapi import FastAPI, Form, File, UploadFile
-from PIL import Image
-import os, io, time, json, easydict, cv2, argparse
+import time, argparse
+
+import easydict, cv2
 import numpy as np
-from yolov7_wrapper import Detector
-from StateMachine import StateMachine
+from typing import Dict
+from datetime import datetime
+from loguru import logger
 from norfair import Video
 
+import app.description as description
+from app.state_machine import StateMachine
+from app.tracking import TrackerWrapper
+from app.yolov7_wrapper import Detector, DetectorInference
+from app.voyager_metadata import YOLO_PT_FILE
 
-model_name = 'basic7_fin.pt' 
-opt = easydict.EasyDict({'agnostic_nms':False, 'augment':True, 'classes':None, 'conf_thres':0.25, 'device':'cpu', 
-                            'exist_ok':False, 'img_size':640, 'iou_thres':0.45, 'name':'exp', 'view_img':False,
-                            'no_trace':False, 'nosave':False, 'project':'runs/detect', 'save_conf':True, 'save_txt':True,
-                            'update':False, 'weights':[model_name]})
+model_name = YOLO_PT_FILE
+opt = easydict.EasyDict(
+    {
+        "agnostic_nms": False,
+        "augment": True,
+        "classes": None,
+        "conf_thres": 0.25,
+        "device": "cpu",
+        "exist_ok": False,
+        "img_size": 640,
+        "iou_thres": 0.45,
+        "name": "exp",
+        "view_img": False,
+        "no_trace": False,
+        "nosave": False,
+        "project": "runs/detect",
+        "save_conf": True,
+        "save_txt": True,
+        "update": False,
+        "weights": [model_name],
+    }
+)
 
 stateMachine = StateMachine()
+tracker = TrackerWrapper()
 detector = Detector(opt)
-result_dict={}  # 세션NO : detection result, 서비스 배포 시 여러장의 이미지를 한꺼번에 추론시키는 경우를 대비해 구축해놓았음.
-                                            # 추후 메모리 누수 막기 위해 초기화시키는 알고리즘 필요
+
+# 세션NO : detection result, 서비스 배포 시 여러장의 이미지를 한꺼번에 추론시키는 경우를 대비해 구축해놓았음.
+# 추후 메모리 누수 막기 위해 초기화시키는 알고리즘 필요
+result_dict: Dict[int, DetectorInference] = {}
 
 
 def test(video_path=None, process_every=30):
@@ -26,16 +52,33 @@ def test(video_path=None, process_every=30):
         if i % process_every != 0:
             continue
 
-        SESSION_NO, rgb = i, cv2.rotate(frame, cv2.ROTATE_180)
+        # if 500 < i < 4000:
+        #     continue
 
-        result_dict[SESSION_NO] = detector.inference(rgb, SESSION_NO, depth_cv=None)  # 리턴타입은 {'yolo':list of bbox}, 바운딩박스 자료형은 딕셔너리   
+        session_no, rgb = i, cv2.rotate(frame, cv2.ROTATE_180)
+        log_str = f"{ datetime.now().strftime('%y%m%d_%H:%M:%S.%f')[:-4] }_Session{session_no}"
 
-        print("세션 아이디: {}", SESSION_NO)
-        print("발견된 물체: {}", [ result['name'] for result in result_dict[SESSION_NO]['yolo'] ])
+        result_dict[session_no] = detector.inference(
+            rgb, session_no, save_name=None, depth_cv=None
+        )  # 리턴타입은 {'yolo':list of bbox}, 바운딩박스 자료형은 딕셔너리
 
-        # StateMachine
-        stateMachine.newFrame(result_dict[SESSION_NO]['yolo'])
-        print("사용자 안내: {}", stateMachine.guides)
+        logger.info("세션 아이디: {}", session_no)
+        logger.info(
+            "발견된 물체: {}",
+            [result["name"] for result in result_dict[session_no]["yolo"]],
+        )
+
+        # Tracking & State Machine
+        tracked_objects = tracker.update(result_dict[session_no].yolo)
+        tracker.save_result(
+            rgb, save_path=f"{detector.save_dir / log_str}_tracking.jpg"
+        )
+
+        stateMachine.newFrame(tracked_objects)
+        guide_enum = stateMachine.guides
+
+        logger.info("사용자 안내: {}", guide_enum)
+
 
 if __name__ == "__main__":
 
@@ -43,4 +86,4 @@ if __name__ == "__main__":
     parser.add_argument("file", type=str, help="Video files to process")
     args = parser.parse_args()
 
-    test(args.file)
+    test(args.file, 15)
